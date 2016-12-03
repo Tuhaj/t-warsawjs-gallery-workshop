066118ffa617bc9c8968826f529f492cfd18b749
Piotr Zientara <zrazic@wp.pl>
2016-12-03 18:42:55 +0100

    creates basic version of the gallery

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log("WarsawJS! Witaj w świecie Pytelewskiego!");
+let displayWindow = document.querySelector('.display-window');
+let images = Array.from(document.querySelectorAll('img'));
+
+function displayImage(imageSource) {
+  let img = document.createElement('img');
+  img.src = imageSource;
+  displayWindow.innerHTML = '';
+  displayWindow.appendChild(img);
+}
+
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    let imageSource = image.parentNode.href;
+    displayImage(imageSource)
+    e.preventDefault();
+  });
+});
+
